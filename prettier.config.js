af2b725697893b4b57722c0fdc197578995e2360
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-18 16:00:58 +0700

    feat: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: true,
+    trailingComma: "es5",
+    tabWidth: 4,
+    singleQuote: false,
+    printWidth: 1000,
+    useTabs: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+    endOfLine: "lf",
+}
