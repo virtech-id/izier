3a69f54d522c48744b46813d3199d8c87f34bb91
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-21 12:01:48 +0700

    feat: use next font inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "../styles/globals.css";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	fallback: ["Arial", "sans-serif", "system-ui"],
+});
 
 export default function RootLayout({
 	children,
@@ -6,7 +12,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.className}>
 			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
