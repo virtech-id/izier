6a420c25cf01564a3840b1f375bc4352e89c438c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-22 12:23:54 +0700

    style: change fragment shader style and color

--- src/component/blob/fragmentShader.ts
+++ src/component/blob/fragmentShader.ts
@@ -1,14 +1,25 @@
 const fragmentShader = `
-    uniform float u_intensity;
-    uniform float u_time;
-
+    uniform vec3 color;
+    uniform float time;
     varying vec2 vUv;
-    varying float vDisplacement;
 
     void main() {
-        float distort = 2.0 * vDisplacement * u_intensity * sin(vUv.y * 10.0 + u_time);
-        vec3 color = vec3(abs(vUv - 0.5) * 2.0  * (1.0 - distort), 1.0);
-        gl_FragColor = vec4(color, 1.0);
+        float distort = vUv.y * sin(vUv.x * 10.0 + time) * 0.1;
+        vec3 baseColor = vec3(0.1, 0.1, 0.1); // blackish color
+        vec3 diffuseColor = vec3(0.0, 0.5 + abs(vUv.y - 0.3) * 2.5, 0.4); // greenish color
+        vec3 specularColor = vec3(0.4, 0.4, 0.4); // white specular color
+        vec3 lightDirection = vec3(0.0, 0.2, 1.3); // light coming from the camera
+
+        vec3 normal = normalize(vec3(vUv - 0.5, distort));
+        vec3 lightReflection = normalize(reflect(lightDirection, normal));
+
+        float shininess = 30.0;
+        float metallic = 0.8;
+        float specularIntensity = pow(max(0.0, dot(lightReflection, vec3(0.0, 0.0, 1.0))), shininess);
+
+        vec3 finalColor = mix(baseColor, diffuseColor, 0.7) + specularColor * specularIntensity;
+
+        gl_FragColor = vec4(mix(vec3(0.0), finalColor, metallic), 1.0);
     }
 `;
 
