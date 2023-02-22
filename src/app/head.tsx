4bb72d59464680d6b93d68da6b9f0b9d41b00586
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-22 13:47:28 +0700

    chore: change title head

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
 	return (
 		<>
-			<title>Create Next App</title>
+			<title>Welcome | Izier</title>
 			<meta
 				content="width=device-width, initial-scale=1"
 				name="viewport"
