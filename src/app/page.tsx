a4f2169023eddbd34ca7a39ddea9ca78b3de536c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-22 15:19:24 +0700

    style: change icon color and hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,14 +82,14 @@ export default function Home() {
 					<Link
 						href="https://github.com/virtech-id/izier"
 						target="_blank"
-						className="transition ease-in-out duration-200 hover:text-grayDark-8"
+						className="transition ease-in-out duration-200 bg-clip-text text-grayDark-10 hover:text-whiteA-12"
 					>
 						<GitHubLogoIcon height={24} width={24} />
 					</Link>
 					<Link
 						href="https://instagram.com/izierapp.id"
 						target="_blank"
-						className="transition ease-in-out duration-200 hover:text-grayDark-8"
+						className="transition ease-in-out duration-200 bg-clip-text text-grayDark-10 hover:text-whiteA-12"
 					>
 						<InstagramLogoIcon height={24} width={24} />
 					</Link>
