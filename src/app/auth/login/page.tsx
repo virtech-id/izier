78fb7fe993b5b6e31e35b7915e1e1524ee4f7a2e
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-23 13:14:25 +0700

    feat(wip): add login email form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 const Login = () => {
 	return (
-		<main className="bg-black">
-			<div className="flex flex-col items-center justify-center h-screen bg-black text-black">
-				Test
+		<main className="flex flex-col items-center justify-center h-screen bg-black">
+			<div className="flex flex-col items-center justify-between m-6">
+				<div className="flex flex-col items-center justify-between space-y-6">
+					<Image
+						src="/izier.svg"
+						alt="Izier Logo"
+						width={96}
+						height={18}
+						priority
+					/>
+					<span className="font-bold text-2xl leading-8">
+						Log in to your account
+					</span>
+				</div>
+				{/* TODO: create the email form */}
+				<div>
+					<form>
+						<label>Email</label>
+						<input></input>
+					</form>
+				</div>
 			</div>
 		</main>
 	);
