c1f09bc9a4d3152e02ee23491c419a7489d61a4e
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-23 09:50:35 +0700

    feat(wip): add login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+	return (
+		<main className="bg-black">
+			<div className="flex flex-col items-center justify-center h-screen bg-black text-black">
+				Test
+			</div>
+		</main>
+	);
+};
+
+export default Login;
