6b94ec28f2dd42ca02dddce6b2e7cd4f5f7f1b1c
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-24 22:20:21 +0700

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,27 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
 import type { ReactElement } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: {
+		default: "Izier",
+		template: "%s | Izier",
+	},
+	description:
+		"Looking to optimize your productivity and accomplish more in less time? Discover Izier - the top-rated productivity tool designed to streamline your work and maximize efficiency. With Izier, you'll have everything you need to work smarter, not harder, and achieve your goals like never before.",
+	applicationName: "Izier",
+	referrer: "same-origin",
+	icons: {
+		icon: "/favicon.ico",
+		apple: "apple-touch-icon.png",
+	},
+	viewport: {
+		width: "device-width",
+		initialScale: 1,
+		maximumScale: 1,
+	},
+};
 
 const inter = Inter({
 	subsets: ["latin"],
