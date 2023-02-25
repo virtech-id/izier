fc4769285d85efce15cee6eb576be75822f8324f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-25 14:21:18 +0700

    feat: add social button items

---
+++ src/app/auth/login/socialButtonLists.ts
@@ -0,0 +1,28 @@
+import { SocialButtonTypes } from "types/socialButton";
+
+export const SOCIAL_BUTTON_ITEMS: SocialButtonTypes[] = [
+	{
+		type: "google",
+		text: "Google",
+		src: "/google.svg",
+		alt: "Google logo",
+	},
+	{
+		type: "github",
+		text: "GitHub",
+		src: "/github.svg",
+		alt: "GitHub logo",
+	},
+	{
+		type: "twitter",
+		text: "Twitter",
+		src: "/twitter.svg",
+		alt: "Twitter logo",
+	},
+	{
+		type: "facebook",
+		text: "Facebook",
+		src: "/facebook.svg",
+		alt: "Facebook logo",
+	},
+];
