d58947037d99ce98e337bb498907f324e83ebabd
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-25 20:26:36 +0700

    refactor: use metadata api from next v13.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,23 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
 
+export const metadata = {
+	title: "Home | Izier",
+	description:
+		"Looking to optimize your productivity and accomplish more in less time? Discover Izier - the top-rated productivity tool designed to streamline your work and maximize efficiency. With Izier, you'll have everything you need to work smarter, not harder, and achieve your goals like never before.",
+	applicationName: "Izier",
+	referrer: "same-origin",
+	icons: {
+		icon: "/favicon.ico",
+		apple: "apple-touch-icon.png",
+	},
+	viewport: {
+		width: "device-width",
+		initialScale: 1,
+		maximumScale: 1,
+	},
+};
+
 const inter = Inter({
 	subsets: ["latin"],
 	fallback: ["Arial", "sans-serif", "system-ui"],
