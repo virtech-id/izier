df998f32c282f5baa8975c9374432f7d81941554
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-25 10:20:34 +0700

    chore: unimplement tailwind debug screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-"use client";
-
 import "../styles/globals.css";
 import { Inter } from "@next/font/google";
-import { useEffect, useState } from "react";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -14,12 +11,6 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const [isDebug, setIsDebug] = useState(false);
-
-	useEffect(() => {
-		setIsDebug(document.body.classList.contains("debug-screens"));
-	}, []);
-
 	return (
 		<html lang="en" className={inter.className}>
 			{/*
@@ -27,7 +18,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
 			<head />
-			<body className={isDebug ? "debug-screens" : ""}>{children}</body>
+			<body>{children}</body>
 		</html>
 	);
 }
