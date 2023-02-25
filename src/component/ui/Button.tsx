bd398e64c61308f9f72a5da1bb8d20e4d1eaea71
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-02-25 10:39:13 +0700

    style(comps): update button style

--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -7,6 +7,7 @@ type ButtonProps = {
 	outlined?: boolean;
 	onClick?: () => void;
 	children: React.ReactNode;
+	fullWidth?: boolean;
 };
 
 const buttonStyles = cva(
@@ -14,13 +15,18 @@ const buttonStyles = cva(
 	{
 		variants: {
 			intent: {
-				default: "bg-grayDark-6 text-grayDark-11 hover:bg-grayDark-8",
-				subtle: "bg-whiteA-12 text-grayDark-1 transition drop-shadow-glow-sm duration-1500 hover:transition hover:drop-shadow-glow-md hover:duration-500",
-				destructive: "bg-redDark-6 text-whiteA-12 hover:bg-redDark-8",
-				ghost: "bg-whiteA-1 text-whiteA-10 hover:bg-whiteA-8 hover:text-whiteA-12",
+				default:
+					"bg-grayDark-6 text-whiteA-12 hover:bg-grayDark-8 whitespace-nowrap",
+				subtle: "bg-whiteA-12 text-grayDark-1 whitespace-nowrap transition drop-shadow-glow-sm duration-2000 hover:transition hover:drop-shadow-glow-md hover:duration-500",
+				destructive:
+					"bg-redDark-6 text-whiteA-12 whitespace-nowrap hover:bg-redDark-8",
+				ghost: "bg-whiteA-1 text-whiteA-10 hover:bg-whiteA-8 whitespace-nowrap hover:text-whiteA-12",
 			},
 			outlined: {
 				true: "border border-grayDark-10",
+			},
+			fullWidth: {
+				true: "w-full",
 			},
 		},
 	}
@@ -31,8 +37,9 @@ const Button: FC<ButtonProps> = ({
 	outlined,
 	onClick,
 	children,
+	fullWidth,
 }) => {
-	const className = buttonStyles({ intent, outlined });
+	const className = buttonStyles({ intent, outlined, fullWidth });
 
 	return (
 		<Toolbar.Root>
